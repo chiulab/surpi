e716a9b4beb96641d7bf0f70579dfd19d2938d05
Scot Federman <sfederman@srp006721srs.ucsf.edu>
2014-07-03 18:27:04 -0700

    added downloading of curated databases adjusted snap indexing to use -O1000

--- create_SURPI_data.sh
+++ create_SURPI_data.sh
@@ -19,13 +19,17 @@
 
 DATE=$(date +%m%d%Y)
 db_dir="NCBI_$DATE"
+curated_dir="curated_$DATE"
 taxonomy_dir="taxonomy"
 RAPSearch_dir="RAPSearch"
+FAST_dir="FAST_SNAP"
 SNAP_nt_dir="SNAP_nt"
 
+#set SNAP index Ofactor. See SNAP documentation for details
+Ofactor=1000
 
-#download NCBI data to $db_dir
-download_SURPI_data.sh -d "$db_dir"
+#download NCBI data to $db_dir, curated data to $curated_dir
+download_SURPI_data.sh -d "$db_dir" -c "$curated_dir"
 
 #
 ##	create taxonomy SQLite dbs and place into $taxonomy_dir
@@ -34,10 +38,10 @@ if [ ! -d "$taxonomy_dir" ]; then
 	mkdir "$taxonomy_dir"
 fi
 
-create_taxonomy_db.sh -d "$db_dir"
-mv gi_taxid_nucl.db "$taxonomy_dir/gi_taxid_nucl_$DATE.db"
-mv gi_taxid_prot.db "$taxonomy_dir/gi_taxid_prot_$DATE.db"
-mv names_nodes_scientific.db "$taxonomy_dir/names_nodes_scientific_$DATE.db"
+# create_taxonomy_db.sh -d "$db_dir"
+# mv gi_taxid_nucl.db "$taxonomy_dir/gi_taxid_nucl_$DATE.db"
+# mv gi_taxid_prot.db "$taxonomy_dir/gi_taxid_prot_$DATE.db"
+# mv names_nodes_scientific.db "$taxonomy_dir/names_nodes_scientific_$DATE.db"
 
 #
 ##create RAPSearch nr db and move into $RAPSearch_dir
@@ -46,12 +50,61 @@ if [ ! -d "$RAPSearch_dir" ]; then
 	mkdir "$RAPSearch_dir"
 fi
 
-pigz -dc -k "$db_directory/nr.gz" > nr
-prerapsearch -d nr -n "nr_db_$DATE"
-mv nr_db "$RAPSearch_dir"
-mv nr_db.info "$RAPSearch_dir"
+# pigz -dc -k "$db_directory/nr.gz" > nr
+# prerapsearch -d nr -n "nr_db_$DATE"
+# mv nr_db "$RAPSearch_dir"
+# mv nr_db.info "$RAPSearch_dir"
+
+#
+## Index curated data
+#
+
+#decompress curated data
+echo "Decompressing curated data..."
+pigz -dc -k "$curated_dir/Bacterial_Refseq_05172012.CLEAN.LenFiltered.uniq.fa.gz" > Bacterial_Refseq_05172012.CLEAN.LenFiltered.uniq.fa
+pigz -dc -k "$curated_dir/hg19_rRNA_mito_Hsapiens_rna.fa.gz" > hg19_rRNA_mito_Hsapiens_rna.fa
+pigz -dc -k "$curated_dir/rapsearch_viral_aa_130628_db_v2.12.fasta.gz" > rapsearch_viral_aa_130628_db_v2.12.fasta
+pigz -dc -k "$curated_dir/viruses-5-2012_trimmedgi-MOD_addedgi.fa.gz" > viruses-5-2012_trimmedgi-MOD_addedgi.fa
+
+echo "Indexing databases..."
+snap index Bacterial_Refseq_05172012.CLEAN.LenFiltered.uniq.fa snap_index_Bacterial_Refseq_05172012.CLEAN.LenFiltered.uniq -s 16 -O$Ofactor
+snap index hg19_rRNA_mito_Hsapiens_rna.fa snap_index_hg19_rRNA_mito_Hsapiens_rna -hg19 -O$Ofactor
+prerapsearch -d rapsearch_viral_aa_130628_db_v2.12.fasta -n "rapsearch_viral_aa_130628_db_v2.12"
+snap index viruses-5-2012_trimmedgi-MOD_addedgi.fa snap_index_viruses-5-2012_trimmedgi-MOD_addedgi -O$Ofactor 
+
+if [ ! -d "$FAST_dir" ]; then
+	mkdir "$FAST_dir"
+fi
+
+mv snap_index_Bacterial_Refseq_05172012.CLEAN.LenFiltered.uniq $FAST_dir
+mv rapsearch_viral_aa_130628_db_v2.12 $RAPSearch_dir
+mv snap_index_viruses-5-2012_trimmedgi-MOD_addedgi $FAST_dir
 
 #
 ## index SNAP-nt
 #
-create_snap_to_nt.sh -n 50 -d "$db_dir"
+# create_snap_to_nt.sh -n 50 -d "$db_dir"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- download_SURPI_data.sh
+++ download_SURPI_data.sh
@@ -28,11 +28,13 @@ red='\e[0;31m'
 endColor='\e[0m'
 DATE=$(date +%m%d%Y)
 destination_dir="NCBI_$DATE"
+curated_dir="curated_$DATE"
 
-while getopts ":hd:" option; do
+while getopts ":hd:c:" option; do
 	case "${option}" in
 		h) HELP=1;;
 		d) destination_dir=${OPTARG};;
+		c) curated_dir=${OPTARG};;
 		:)	echo "Option -$OPTARG requires an argument." >&2
 			exit 1
       		;;
@@ -54,6 +56,9 @@ ${bold}Command Line Switches:${normal}
 	-d	Specify directory to create for downloaded data
 		(optional. If unsupplied, will default to NCBI_[current date] )
 
+	-c	Specify directory to create for curated data
+		(optional. If unsupplied, will default to curated_[current date] )
+
 ${bold}Usage:${normal}
 
 $scriptname -d NCBI_07022014
@@ -63,6 +68,7 @@ USAGE
 fi
 
 NCBI="ftp://ftp.ncbi.nih.gov/"
+chiulab_dir="http://chiulab.ucsf.edu/SURPI/databases"
 FASTA_dir="blast/db/FASTA/"
 TAXONOMY_dir="pub/taxonomy/"
 
@@ -99,6 +105,10 @@ download_file ()
 		fi
 	fi
 }
+
+#
+## Download NCBI data
+#
 
 if [ ! -d "$destination_dir" ]; then
 	mkdir "$destination_dir"
@@ -138,5 +148,24 @@ if [ ! -f "$destination_dir/$gi_taxid_prot" ]; then
 else
 	echo "$gi_taxid_prot already present."
 fi
+
+#
+## Download Chiulab curated data
+#
+declare -a download_list=("Bacterial_Refseq_05172012.CLEAN.LenFiltered.uniq.fa.gz" "hg19_rRNA_mito_Hsapiens_rna.fa.gz" "rapsearch_viral_aa_130628_db_v2.12.fasta.gz" "viruses-5-2012_trimmedgi-MOD_addedgi.fa.gz" )
+
+if [ ! -d "$curated_dir" ]; then
+	mkdir "$curated_dir"
+fi
+
+for download in "${download_list[@]}"
+do
+	if [ ! -f "$curated_dir/$download" ]; then
+		echo "Downloading $download"
+		download_file "$curated_dir" "$chiulab_dir" "$download" "$download.md5"
+	else
+		echo "$download already present."
+	fi
+done
 
 echo -e "${green}Download complete.${endColor}"
